47c012d2547d1e66d76f9ae9326e71f9fa707b70
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-01-09 20:24:16 -0300

    merge(develop): setup repository
    chore: add gitignore
    docs(readme): add fcc logo and description
    refactor: clean files

--- test_module.py
+++ test_module.py
@@ -2,9 +2,9 @@ import unittest
 import budget
 from budget import create_spend_chart
 
-
 class UnitTests(unittest.TestCase):
     maxDiff = None
+
     def setUp(self):
         self.food = budget.Category("Food")
         self.entertainment = budget.Category("Entertainment")
@@ -101,5 +101,5 @@ class UnitTests(unittest.TestCase):
         expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
         self.assertEqual(actual, expected, 'Expected different chart representation. Check that all spacing is exact.')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()

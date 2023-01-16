bc04e9d0181a58d855c24a0b33d7d7ef73ea3c77
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-01-16 19:50:34 -0300

    merge(develop): add monetary operations
    feat(budget): add transfer method
    feat(budget): add deposit and withdraw methods
    feat(budget): add get_balance and check_funds
    feat(budget): add instance variables and methods

--- budget.py
+++ budget.py
@@ -1,3 +1,44 @@
 class Category:
+    def __init__(self, category):
+        self.category = category
+        self.ledger = list()
+
+    def deposit(self, amount, description=str()):
+        self.ledger.append({
+            'amount': amount,
+            'description': description
+        })
+
+    def withdraw(self, amount, description=str()):
+        funds = bool(self.check_funds(amount))
+
+        if funds:
+            self.ledger.append({
+                'amount': -amount,
+                'description': description
+            })
+        
+        return funds
+
+    def get_balance(self):
+        balance = float()
+
+        for operation in self.ledger:
+            balance += operation['amount']
+
+        return balance
+
+    def transfer(self, amount, destination):
+        funds = bool(self.check_funds(amount))
+        
+        if funds:
+            self.withdraw(amount, 'Transfer to ' + destination.category)
+            destination.deposit(amount, 'Transfer from ' + self.category)
+        
+        return funds
+
+    def check_funds(self, amount):
+        return True if amount <= self.get_balance() else False
 
 def create_spend_chart(categories):
+    return 0

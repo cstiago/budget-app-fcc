69b1707396ccfd401e84a8f6bc524552cf100acd
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-01-16 19:50:34 -0300

    feat(budget): add instance variables and methods

--- budget.py
+++ budget.py
@@ -1,3 +1,22 @@
 class Category:
+    def __init__(self, category):
+        self.category = category
+        self.ledger = list()
+
+    def deposit(self, amount, description=str()):
+        return 0
+
+    def withdraw(self, amount, description=str()):
+        return 0
+
+    def get_balance(self):
+        return 0
+
+    def transfer(self, amount, destination):
+        return 0
+
+    def check_funds(self, amount):
+        return 0
 
 def create_spend_chart(categories):
+    return 0

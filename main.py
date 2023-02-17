c237d66a55133fa439afc8a479c40e1903be0acc
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-02-17 15:56:05 -0300

    feat(main): clean example

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-from unittest import main
 import budget
 from budget import create_spend_chart
+from unittest import main
 
-food = budget.Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
+food = budget.Category('Food')
+food.deposit(100, 'Initial deposit')
+food.withdraw(10, 'Purchase 1')
+food.withdraw(29.99, 'Purchase 2')
 
-print(food.get_balance())
+clothing = budget.Category('Clothing')
+food.transfer(30, clothing)
+clothing.withdraw(10.25, 'Purchase 1')
+clothing.withdraw(15, 'Purchase 2')
 
-clothing = budget.Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = budget.Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
+auto = budget.Category('Auto')
+auto.deposit(500, 'Initial deposit')
+auto.withdraw(125.50, 'Purchase 1')
+auto.withdraw(9.99, 'Purchase 2')
 
 print(food)
 print(clothing)
+print(auto)
 
 print(create_spend_chart([food, clothing, auto]))
 
